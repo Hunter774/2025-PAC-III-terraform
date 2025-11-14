0fe5238e12fb8ee8d88b2dc946ab5ae8ad9ba301
Roberto Matamoros <65750735+Hunter774@users.noreply.github.com>
2025-11-14 15:04:34 -0600

    Define location and resource group name variables
    Add variables for location and resource group name

--- variables.tf
+++ variables.tf
@@ -1,1 +1,11 @@
 
+variable "location" {
+  description = "Region (despues lo cambio :v)"
+  type        = string
+  default     = "eastus2"
+}
+
+variable "resource_group_name" {
+  description = "Nombre del resource group principal (Despues lo cambio :v)"
+  type        = string
+}
